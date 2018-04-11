cb2c95336cf8c7d50ec15ae68136adb6c84a19c6
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-11 17:16:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// index.js
 
+//is it right to write 2 'module.exports'?
 module.exports = makeGitterURL
 module.exports = makeGitterChatbox
 
+// function No.1
 function makeGitterURL (username) {
   return `https://gitter.im/${username}/~embed`
 }
 
+// function No.2
 function makeGitterChatbox (profile) {
 		  return bel`
 		    <div class=${css1.chatbox}>
