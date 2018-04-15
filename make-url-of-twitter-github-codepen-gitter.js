c34c36b670034782f2c304b9d727b95936edb0fe
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-15 15:06:10 +0800

    Create make-url-of-twitter-github-codepen-gitter.js

---
+++ make-url-of-twitter-github-codepen-gitter.js
@@ -0,0 +1,9 @@
+module.exports = makeGitterURL2
+
+function makeLinkURL2 (username) {
+  var twitter           = `https://twitter.com/${username}`
+  var github            = `https://github.com/${username}`
+  var codepen           = `https://codepen.io/${username}`
+  
+  var gitter           =  `https://gitter.im/${username}/~embed`
+  }
