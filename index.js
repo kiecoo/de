00ff568fa6f3827bae8a5f35f12d9a71490a0813
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-15 08:17:56 +0800

    v1(success)-without module

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 // index.js
 
-//is it right to write 2 'module.exports'?
-module.exports = makeGitterURL
-module.exports = makeGitterChatbox
-
-// function No.1
-function makeGitterURL (username) {
-  return `https://gitter.im/${username}/~embed`
-}
-
-// function No.2
-function makeGitterChatbox (profile) {
-		  return bel`
-		    <div class=${css1.chatbox}>
-		<img src="${profile.photo}" class=${css1.photo1}>
-		      <h1> ${profile.name} </h1>
-		      <iframe class=${css1.iframe} src=${makeGitterURL1(profile.username)}></iframe>
-		    </div>
-		  `
-		}
+module.exports =cardCombineGitter   
     
     
