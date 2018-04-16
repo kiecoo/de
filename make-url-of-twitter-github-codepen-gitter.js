a716209f920eb664316a1adbb7802ab0be8a39c1
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-16 13:34:51 +0800

    change module content (4-links into 1-link-gitter)

--- make-url-of-twitter-github-codepen-gitter.js
+++ make-url-of-twitter-github-codepen-gitter.js
@@ -1,9 +1,5 @@
 module.exports = makeLinkURL2
 
 function makeLinkURL2 (username) {
-  var twitter           = `https://twitter.com/${username}`
-  var github            = `https://github.com/${username}`
-  var codepen           = `https://codepen.io/${username}`
-  
-  var gitter           =  `https://gitter.im/${username}/~embed`
+  return `https://gitter.im/${username}/~embed`
   }
